5d2e0f065d65a7ef5da4ede5a4b55e49f7776b8d
w1t3chlyyy <w1t3chlyyy@mail.ru>
2026-08-20 16:17:11 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://enuhrsbphkhmigoxvegg.supabase.co';
+const supabaseAnonKey = 'sb_publishable_SeMxICcl9Fm5n8wxJoMM3Q_iHdsAfLR';
 
 console.log('Supabase URL:', supabaseUrl ? 'задан' : 'НЕ задан');
 console.log('Supabase Key:', supabaseAnonKey ? 'задан' : 'НЕ задан');
